0a406dcef9083ed11b3898f8d4c5f57407093337
niloysannyal <61078546+niloysannyal@users.noreply.github.com>
2025-04-16 01:54:48 +0600

    Update util.py

--- server/util.py
+++ server/util.py
@@ -5,7 +5,7 @@ import numpy as np
 import base64
 import cv2
 from server import wavelet
-from wavelet import w2d
+from server.wavelet import w2d
 
 __class_name_to_number = {}
 __class_number_to_name = {}
